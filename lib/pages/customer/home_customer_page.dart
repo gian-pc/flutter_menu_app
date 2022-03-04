faf8eeabf09c0d1cb8f20fb58c582ce11ac8587c
Gian Carlos <gpaucarcortez@gmail.com>
2022-03-04 18:28:32 -0500

    first commit

---
+++ lib/pages/customer/home_customer_page.dart
@@ -0,0 +1,296 @@
+// ignore_for_file: use_key_in_widget_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class HomeCustomerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff1B1A21),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Header
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage("assets/images/logo.jpg"),
+                    ),
+                    SizedBox(width: 8.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Central Palace",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Source Sans Pro Black'),
+                        ),
+                        Text(
+                          "Un sitio para tus mejores experiencias.",
+                          style: TextStyle(
+                            color: Colors.white60,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Expanded(child: Container()),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+
+              // Categories
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        child: FilterChip(
+                          backgroundColor: Color(0xffED9B00),
+                          label: Container(
+                            height: 26,
+                            width: 50,
+                            child: Center(
+                              child: Text(
+                                "Todos",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                          onSelected: (bool selected) {},
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        child: FilterChip(
+                          backgroundColor: Color(0xff1B1A21),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24.0),
+                            side: BorderSide(
+                              color: Color(0xff1B1A21),
+                            ),
+                          ),
+                          label: Container(
+                            height: 26.0,
+                            child: Center(
+                              child: Text(
+                                "Postres",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white60,
+                                ),
+                              ),
+                            ),
+                          ),
+                          onSelected: (bool selected) {},
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        child: FilterChip(
+                          backgroundColor: Color(0xff1B1A21),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24.0),
+                            side: BorderSide(
+                              color: Color(0xff1B1A21),
+                            ),
+                          ),
+                          label: Container(
+                            height: 26.0,
+                            child: Center(
+                              child: Text(
+                                "Bebidas",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white60,
+                                ),
+                              ),
+                            ),
+                          ),
+                          onSelected: (bool selected) {},
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        child: FilterChip(
+                          backgroundColor: Color(0xff1B1A21),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24.0),
+                            side: BorderSide(
+                              color: Color(0xff1B1A21),
+                            ),
+                          ),
+                          label: Container(
+                            height: 26.0,
+                            child: Center(
+                              child: Text(
+                                "Entradas",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white60,
+                                ),
+                              ),
+                            ),
+                          ),
+                          onSelected: (bool selected) {},
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        child: FilterChip(
+                          backgroundColor: Color(0xff1B1A21),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24.0),
+                            side: BorderSide(
+                              color: Color(0xff1B1A21),
+                            ),
+                          ),
+                          label: Container(
+                            height: 26.0,
+                            child: Center(
+                              child: Text(
+                                "Platos de Fondo",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white60,
+                                ),
+                              ),
+                            ),
+                          ),
+                          onSelected: (bool selected) {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Promotions
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: 200.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 140,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18.0),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 4),
+                            blurRadius: 10,
+                            color: Colors.white.withOpacity(0.2),
+                          ),
+                        ],
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                "https://www.fopesa.com.pe/wp-content/uploads/2020/08/47-fosforera-peruana-inti-1.jpg")),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            right: 0,
+                            child: Container(
+                              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+                              decoration: BoxDecoration(
+                                color: Color(0xffFFCC00),
+                                borderRadius: BorderRadius.circular(26.0),
+                              ),
+                              child: Text(
+                                "30% Desc",
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Costillar de Cordero",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "Costillar de Cordero con papas nativas Costillar de Cordero con papas nativas",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 13.0,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "S/. 30",
+                          style: TextStyle(
+                            color: Colors.white54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: [
+                              Text(
+                                "4.6",
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 18.0,
+                                color: Color(0xffED9B00),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
