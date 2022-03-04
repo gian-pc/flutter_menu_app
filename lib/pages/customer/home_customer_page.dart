4e97cd581e0e0f39681def899638b68da97e552a
Gian Carlos <gpaucarcortez@gmail.com>
2022-03-04 18:47:10 -0500

    Card Promotions 2

--- lib/pages/customer/home_customer_page.dart
+++ lib/pages/customer/home_customer_page.dart
@@ -185,105 +185,327 @@ class HomeCustomerPage extends StatelessWidget {
                 ),
               ),
 
+              SizedBox(
+                height: 20.0,
+              ),
               // Promotions
-              Container(
-                constraints: BoxConstraints(
-                  maxWidth: 200.0,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
                   children: [
                     Container(
-                      height: 140,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(18.0),
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 4),
-                            blurRadius: 10,
-                            color: Colors.white.withOpacity(0.2),
-                          ),
-                        ],
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(
-                                "https://www.fopesa.com.pe/wp-content/uploads/2020/08/47-fosforera-peruana-inti-1.jpg")),
+                      margin: EdgeInsets.only(left: 10.0, right: 10.0),
+                      constraints: BoxConstraints(
+                        maxWidth: 200.0,
                       ),
-                      child: Stack(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Positioned(
-                            right: 0,
-                            child: Container(
-                              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
-                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
-                              decoration: BoxDecoration(
-                                color: Color(0xffFFCC00),
-                                borderRadius: BorderRadius.circular(26.0),
-                              ),
-                              child: Text(
-                                "30% Desc",
-                                style: TextStyle(
-                                  fontSize: 15.0,
-                                  fontWeight: FontWeight.bold,
+                          Container(
+                            height: 140,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(18.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(0, 4),
+                                  blurRadius: 10,
+                                  color: Colors.white.withOpacity(0.2),
                                 ),
-                              ),
+                              ],
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      "https://www.fopesa.com.pe/wp-content/uploads/2020/08/47-fosforera-peruana-inti-1.jpg")),
+                            ),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  right: 0,
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(
+                                        horizontal: 10.0, vertical: 4.0),
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 3.0),
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffFFCC00),
+                                      borderRadius: BorderRadius.circular(26.0),
+                                    ),
+                                    child: Text(
+                                      "30% Desc",
+                                      style: TextStyle(
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Costillar de Cordero",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      "Costillar de Cordero con papas nativas Costillar de Cordero con papas nativas",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 13.0,
-                      ),
-                    ),
-                    SizedBox(height: 4),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "S/. 30",
-                          style: TextStyle(
-                            color: Colors.white54,
-                            fontWeight: FontWeight.bold,
+                          SizedBox(height: 10),
+                          Text(
+                            "Costillar de Cordero",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
                           ),
-                        ),
-                        Container(
-                          child: Row(
+                          SizedBox(height: 4),
+                          Text(
+                            "Costillar de Cordero con papas nativas Costillar de Cordero con papas nativas",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Text(
-                                "4.6",
+                                "S/. 30",
                                 style: TextStyle(
                                   color: Colors.white54,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Icon(
-                                Icons.star,
-                                size: 18.0,
-                                color: Color(0xffED9B00),
+                              Container(
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "4.6",
+                                      style: TextStyle(
+                                        color: Colors.white54,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      size: 18.0,
+                                      color: Color(0xffED9B00),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
+                    Container(
+                      margin: EdgeInsets.only(left: 10.0, right: 10.0),
+                      constraints: BoxConstraints(
+                        maxWidth: 200.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 140,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(18.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(0, 4),
+                                  blurRadius: 10,
+                                  color: Colors.white.withOpacity(0.2),
+                                ),
+                              ],
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      "https://www.fopesa.com.pe/wp-content/uploads/2020/08/47-fosforera-peruana-inti-1.jpg")),
+                            ),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  right: 0,
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(
+                                        horizontal: 10.0, vertical: 4.0),
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 3.0),
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffFFCC00),
+                                      borderRadius: BorderRadius.circular(26.0),
+                                    ),
+                                    child: Text(
+                                      "30% Desc",
+                                      style: TextStyle(
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Costillar de Cordero",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            "Costillar de Cordero con papas nativas Costillar de Cordero con papas nativas",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "S/. 30",
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Container(
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "4.6",
+                                      style: TextStyle(
+                                        color: Colors.white54,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      size: 18.0,
+                                      color: Color(0xffED9B00),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 10.0, right: 10.0),
+                      constraints: BoxConstraints(
+                        maxWidth: 200.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 140,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(18.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(0, 4),
+                                  blurRadius: 10,
+                                  color: Colors.white.withOpacity(0.2),
+                                ),
+                              ],
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      "https://www.fopesa.com.pe/wp-content/uploads/2020/08/47-fosforera-peruana-inti-1.jpg")),
+                            ),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  right: 0,
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(
+                                        horizontal: 10.0, vertical: 4.0),
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 3.0),
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffFFCC00),
+                                      borderRadius: BorderRadius.circular(26.0),
+                                    ),
+                                    child: Text(
+                                      "30% Desc",
+                                      style: TextStyle(
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Costillar de Cordero",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            "Costillar de Cordero con papas nativas Costillar de Cordero con papas nativas",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "S/. 30",
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Container(
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "4.6",
+                                      style: TextStyle(
+                                        color: Colors.white54,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      size: 18.0,
+                                      color: Color(0xffED9B00),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
                   ],
                 ),
               ),
