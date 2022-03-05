3fed502985e242d9ef171d951773cbdeba43ddb2
Gian Carlos <gpaucarcortez@gmail.com>
2022-03-05 18:18:56 -0500

    Product Detail

---
+++ lib/pages/customer/product_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product Detail Page"),
+      ),
+      body: Center(),
+    );
+  }
+}
