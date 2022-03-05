08e56635d695eabb28c07982f13b138d403d0709
Gian Carlos <gpaucarcortez@gmail.com>
2022-03-05 17:38:35 -0500

    Category List2

--- lib/pages/customer/category_list_product_page.dart
+++ lib/pages/customer/category_list_product_page.dart
@@ -13,7 +13,10 @@ class CategoryListProductPage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search),),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
         ],
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -22,8 +25,68 @@ class CategoryListProductPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-
-
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14.0),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "https://www.clara.es/medio/2021/11/28/postres-navidenos_3f462fd7_1280x1115.jpg"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Torta de de frutas",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          SizedBox(height: 4.0),
+                          Text(
+                            "Torta helada a base de frutas de estación y acompañado de nueces deliciosas",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.white60,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          SizedBox(height: 2.0),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Color(0xffED9B00),
+                                size: 16.0,
+                              ),
+                              Text(
+                                "4.5",
+                                style: TextStyle(
+                                  color: Colors.white60,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
