30972e9777d7f0a48a36481416bcd735ca42c40d
Gian Carlos <gpaucarcortez@gmail.com>
2022-03-06 21:20:30 -0500

    Product Detail Page

--- lib/pages/customer/product_detail_page.dart
+++ lib/pages/customer/product_detail_page.dart
@@ -1,14 +1,117 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo3_menu_app/common/colors.dart';
 
 class ProductDetailPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff1B1A21),
       appBar: AppBar(
-        title: Text("Product Detail Page"),
+        title: Text(
+          "Detalle del Producto",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: Center(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 340,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://cocimaniacos.com/wp-content/uploads/2017/08/0_142eb1_3fca5e2e_orig.jpg"),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xff1B1A21),
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment(0, 0),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Costillar de Cordero",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "S/. 50.00",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.timer,
+                                    size: 16,
+                                    color: Colors.white70,
+                                  ),
+                                  SizedBox(width: 3.0),
+                                  Text(
+                                    "10 min",
+                                    style: TextStyle(
+                                        color: Colors.white70,
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(width: 10.0),
+                                  Icon(
+                                    Icons.star,
+                                    size: 16,
+                                    color: BRAND_PRIMARY,
+                                  ),
+                                  SizedBox(width: 3.0),
+                                  Text(
+                                    "4.5",
+                                    style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
